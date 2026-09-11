ca85c127e7038c2369f4a6bcfadde60ddc049ef8
mahmudov7 <abdullomahmudow7777@gmail.com>
2026-09-11 16:10:52 +0300

    Add files via upload

---
+++ 6day/Program.cs
@@ -0,0 +1,55 @@
+﻿//task 1
+// int n=Convert.ToInt32(Console.ReadLine());
+// for(int i=1; i<=n; i++)
+// {
+//     System.Console.Write($"{i} ");
+// }
+//task 2
+// int n=Convert.ToInt32(Console.ReadLine());
+// for(int i=n; i>=1; i--)
+// {
+//     System.Console.Write($"{i} ");
+// }
+//task3
+// int n=Convert.ToInt32(Console.ReadLine());
+// int s=0;
+// for(int i=n; i>=1; i--)
+// {
+//    s+=i;
+// }
+// task4
+// int n=Convert.ToInt32(Console.ReadLine());
+// int a=0;
+// for(int i=n; i>0; i/=10)
+// {
+//     System.Console.Write($"{i%10} ");
+// }
+//task 5
+// int a=Convert.ToInt32(Console.ReadLine());
+// int s=0;
+// int w=0;
+// for(int i=a; i>0; i/=10)
+// {
+// w=i%10;
+//     s++;
+// }
+// System.Console.WriteLine(s);
+//task 6
+// int a=Convert.ToInt32(Console.ReadLine());
+// int b=Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine($"All even numbers from {a} to {b} are: ");
+// for(int i=a; i<=b; i++)
+// {
+//     if (i % 2 == 0)
+//     {
+//         System.Console.Write($"{i} ");
+//     }
+// }
+// System.Console.WriteLine($"All odd numbers from {a} to {b} are: ");
+// for(int i=a; i<=b; i++)
+// {
+//     if (i % 2 != 0)
+//     {
+//         System.Console.Write($"{i} ");
+//     }
+// }
